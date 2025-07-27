e8946fe5b137578b43e0c8c83d085082724d22d9
HKMUSIC <himanshu4hk9@gmail.com>
2025-07-27 12:15:25 +0530

    Update couples.py

--- SHUKLAMUSIC/plugins/tools/couples.py
+++ SHUKLAMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SHUKLAMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="❖ ᴘᴏᴡᴇʀᴇᴅ ➥ 愛 | 𝗦𝗧么𝗟𝗞𝚵𝗥 ",
-            url=f"https://t.me/HEHE_STALKER",
+            text="❖ ᴘᴏᴡᴇʀᴇᴅ ➥ 𓆩𝐆𐑴𝐣𝛐 𝐈𝐧𝐟𝛊𝐧𝛊𝐭𝐲𓆪 ",
+            url=f"https://t.me/II_YOUR_GOJO_ll",
         ),
     ],
 ]
