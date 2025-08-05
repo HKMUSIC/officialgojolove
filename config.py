a46a1f897edd4077cf5f2e6c417b7c6a985c56fc
HKMUSIC <himanshu4hk9@gmail.com>
2025-08-05 17:52:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Support
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GOJO_SUPPORT_GROUP_II")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/GOJO_NOBITA_II")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+w_H0IbfCUL85ZTM1")
 
 # Assistant settings
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
